0592f99bfa13fef4f84025f3c562152e751beeb2
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2024-05-17 13:40:19 +0900

    [add] xilinx-debug drivers source files

---
+++ drivers/media/platform/xilinx/insmod-xilinx-debug.sh
@@ -0,0 +1,7 @@
+modprobe videobuf2-dma-contig
+insmod   xilinx-debug-video.ko
+insmod   xilinx-debug-csi2rxss.ko
+insmod   xilinx-debug-demosaic.ko
+insmod   xilinx-debug-gamma.ko
+insmod   xilinx-debug-vpss-csc.ko
+insmod   xilinx-debug-vpss-scaler.ko
