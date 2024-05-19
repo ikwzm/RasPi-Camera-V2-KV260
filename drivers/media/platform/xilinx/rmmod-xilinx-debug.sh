a3e37c13379a496260e0843eb3c0cdd3e6fa3d5d
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2024-05-19 17:12:36 +0900

    [update] drivers/media/platform/xilinx/insmod-xilinx-debug.sh

--- drivers/media/platform/xilinx/rmmod-xilinx-debug.sh
+++ drivers/media/platform/xilinx/rmmod-xilinx-debug.sh
@@ -1,6 +1,6 @@
-rmmod   xilinx-debug-video
 rmmod   xilinx-debug-csi2rxss
 rmmod   xilinx-debug-demosaic
 rmmod   xilinx-debug-gamma
 rmmod   xilinx-debug-vpss-csc
 rmmod   xilinx-debug-vpss-scaler
+rmmod   xilinx-debug-video
